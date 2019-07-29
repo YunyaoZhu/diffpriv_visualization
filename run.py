48d3fec4ff3728c97a61e4cfa23ebcec3e633253
Yunyao Zhu <yunyaozhu@Yunyaos-MacBook-Air.local>
2019-07-29 13:57:14 -0400

    Fix name issue

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ app.layout = html.Div(children=[
         id='example-graph',    
         figure={
             'data': [
-                {'x': df.eps.values, 'y': dp.auc.values, 'type': 'bar', 'name': 'SF'}
+                {'x': df.eps.values, 'y': df.auc.values, 'type': 'bar', 'name': 'SF'}
 #                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
 #                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
