6bffc387f0ca977c0dffcde11f1da4d574a84e40
Yunyao Zhu <yunyaozhu@Yunyaos-MacBook-Air.local>
2019-07-29 14:12:58 -0400

    Add color dict and title row

--- run.py
+++ run.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+'''Import Calls'''
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,15 +12,16 @@ from dash.dependencies import Input, Output
 from flask import Flask
 import os
 import pathlib
-
 import pandas as pd
 
+'''Data path'''
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
 
 
 df = pd.read_csv(DATA_PATH.joinpath('results_output_r_5runs_35716rows_balance_action.csv.csv'))
 
+'''Initialize Dash'''
 server = Flask(__name__)
 server.secret_key = os.environ.get('secret_key', 'secret')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -27,7 +29,69 @@ app = dash.Dash(__name__, server = server, external_stylesheets=external_stylesh
 # app = dash.Dash(name = __name__, server = server)
 app.config.supress_callback_exceptions = True
 
+'''Define colors'''
+colors = {
+    'background': '#111111',
+    'title': '#02140F',
+    'text': '#00A676'
+}
+
+'''App layout'''
 app.layout = html.Div(children=[
+    # title row
+    html.Div([
+        html.Div([
+            html.Img(src="http://www2.cs.duke.edu/web_resources/logo/square/blue/cslogo_square_blue.SMALL.png",
+                 style={
+#                     'height': '90%',
+#                     'width': '90%',
+                     "height": "120px",
+                    "width": "auto",
+                    'float': 'left',
+                 },
+            ),
+        ], className='two columns',),
+        
+        html.Div([
+            html.H5(children='Protecting Individual Privacy', 
+                style={
+                    'textAlign': 'center',
+                    'color': colors['title'],
+                    'margin-top': 30,
+                    'margin-bottom': 0, 
+                    'font-family': 'Helvetica, monospace',
+                    'font-size': 40
+                    },
+                ),
+            html.H6(children='Using Differential Privacy', 
+                style={
+                    'textAlign': 'center',
+                    'color': colors['title'],
+                    'margin-top': 0,
+                    'font-family': 'Helvetica, monospace',
+                    'font-size': 20
+                    },
+                )
+        ], className='eight columns'),
+        
+        
+        html.Div([
+            html.A(html.Button("Github", id="github-button"),
+                   href="https://github.com/cmzou/diffpriv",
+                   style={
+                       'float': 'right',
+                       'margin-top': 30
+                   }
+            ),
+        ], className='two columns'),
+               
+        
+    ], className='row'),
+    
+    
+    
+    
+    
     html.H1(children='Hello Dash'),
     html.Div(children='''
         Dash: A web application framework for Python.
@@ -49,6 +113,7 @@ app.layout = html.Div(children=[
     html.Div(id='my-div')
 ])
 
+'''Callbacks'''
 @app.callback(
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
