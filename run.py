a03cde90e1f3ef508e2ea2000d1fff287b0d2cac
Yunyao Zhu <yunyaozhu@Yunyaos-MacBook-Air.local>
2019-07-29 23:40:15 -0400

    Remove comments

--- run.py
+++ run.py
@@ -92,14 +92,8 @@ app.layout = html.Div(style={'font-family': 'Helvetica, monospace'}, children=[
     
     html.Div([
         # Control panel
-        html.Div(style={
-#                         'backgroundColor': colors['white'], 
-#                         'padding-left': 20, 
-#                         'padding-top': 20
-                       }, 
-                 children=[
-            
-            
+        html.Div(children=[
+            # Settings and explanations tabs
             html.Div([
                 dcc.Tabs(id="tabs", value='tab-1', children=[
                     dcc.Tab(label='Graph settings', value='tab-1'),
@@ -107,12 +101,6 @@ app.layout = html.Div(style={'font-family': 'Helvetica, monospace'}, children=[
                 ]),
                 html.Div(id='tabs-content')
             ]),
-
-            
-            
-             
-
-
         ], className='five columns'),
         
         # AUC graph
@@ -127,78 +115,20 @@ app.layout = html.Div(style={'font-family': 'Helvetica, monospace'}, children=[
            dcc.Graph(id='sex_di'),
 
         ], className='seven columns'),
-        
-        
-        
     ], className='row'),
-    
-    
-    # DI for race, ethnicity, and sex
-#     html.Div([
-        
-#         html.Div([
-#             dcc.Graph(id='race_di')
-#         ], className='four columns'),
-        
-#         html.Div([
-#             dcc.Graph(id='eth_di',)
-#         ], className='four columns'),
-        
-#         html.Div([
-#             dcc.Graph(id='sex_di')
-#         ], className='four columns'),
-        
-#     ], className='row'),
-    
-    
-    
-    # checklist (di metrics)
-#     html.Div([
-#         dcc.Checklist(
-#             id = 'metric-checklist',
-#             options=[
-#                 {'label': 'Approval', 'value': 'approval'},
-#                 {'label': 'False Negative', 'value': 'fn'},
-#                 {'label': 'False Positive', 'value': 'fp'}
-#             ],
-#             values=['approval'],
-#             labelStyle={'display': 'inline-block',
-#                        'margin-top': 20,
-#                         'font-family': 'Helvetica, monospace',
-#                           'font-size': 20}
-#         ) 
-#     ], className='container-fluid'),
-
-
 ], className='ten columns offset-by-one')
 
 '''Callbacks'''
-# @app.callback(
-#     dash.dependencies.Output('output-container', 'children'),
-#     [dash.dependencies.Input('my-dropdown', 'value')])
-# def update_output(value):
-#     return 'You have selected "{}"'.format(value)
-
-# callback for overall auc
-
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs', 'value')])
 def render_content(tab):
     if tab == 'tab-1':
         return html.Div(style={'padding-top': 20, 'font-family': 'Helvetica, monospace', 'font-size': 16}, children=[
-#             html.P(
-#                 "Choose dataset:",
-#                 style={'font-family': 'Helvetica, monospace',
-#                       'font-size': 20,
-#                       'font-weight': 'bold'}
-#             ),
             html.H6(
                 'Choose dataset(s)',
                 style={'font-family': 'Helvetica, monospace',
                       'font-size': 20,
-#                       'font-weight': 'bold'
-                      }
-                
+                      }                
             ),
 
             dcc.Dropdown(
@@ -230,24 +160,14 @@ def render_content(tab):
 
 
                 multi=True,
-#                 value='results_output_r_5runs_35716rows_balance_action.csv.csv'
                 placeholder="Select dataset(s)",
             ),
-            
-#             html.P(
-#                 "x-axis type:",
-#                 style={'margin-top': 25,
-#                       'font-family': 'Helvetica, monospace',
-#                       'font-size': 20,
-#                       'font-weight': 'bold'}
-#             ),
             
             html.H6(
                 'X-axis type:',
                 style={'font-family': 'Helvetica, monospace',
                       'font-size': 20,
                        'margin-top': 30,
-#                       'font-weight': 'bold'
                       }
                 
             ),
@@ -260,19 +180,12 @@ def render_content(tab):
                            'font-family': 'Helvetica, monospace',
                           'font-size': 16}
             ),
-#             html.P(
-#                 "Disparte impact metric:",
-#                 style={'margin-top': 25,
-#                       'font-family': 'Helvetica, monospace',
-#                       'font-size': 20,
-#                       'font-weight': 'bold'}
-#             ),
+
             html.H6(
                 'Disparte Impact (DI) metric:',
                 style={'font-family': 'Helvetica, monospace',
                       'font-size': 20,
                        'margin-top': 30,
-#                       'font-weight': 'bold'
                       }
                 
             ),
@@ -295,7 +208,6 @@ def render_content(tab):
                 'AUC',
                 style={'font-family': 'Helvetica, monospace',
                       'font-size': 20,
-#                       'font-weight': 'bold'
                       }
                 
             ),
@@ -313,7 +225,6 @@ def render_content(tab):
                 style={'font-family': 'Helvetica, monospace',
                       'font-size': 20,
                        'margin-top': 20,
-#                       'font-weight': 'bold'
                       }
                 
             ),
@@ -420,9 +331,7 @@ def update_image_src(dataset_filename, dataset_options, metric_checklist, xaxis_
     figure = {
         'data': data,
         'layout': {
-#             'margin': dict(l=0, r=0, b=0, t=0, pad=0),
             'height': 500,
-#              'margin': { l: 5, r: 5, b: 5, t: 5, pad: 10},
             'title': 'Race Disparate Impact',
             'xaxis' : dict(
                 title='Epsilons',
@@ -442,26 +351,12 @@ def update_image_src(dataset_filename, dataset_options, metric_checklist, xaxis_
                 color='#7f7f7f'
                 )
             ),
-#             'shape': [{type="rect",
-#                         x0=0.1,
-#                         y0=0.8,
-#                         x1=2,
-#                         y1=1.2,
-#                         line=dict(
-#                             color="RoyalBlue",
-#                             width=2,
-#                         ),
-#                         fillcolor="LightSkyBlue",
-#                     }
-#                 ],
+
             'legend_orientation':"h",
             'legend':dict(x=0, y=-0.8)
         }
     }
     return figure
-
-
-
 
 
 # callback for ethnicity di
