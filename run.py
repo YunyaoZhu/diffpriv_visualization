32b2a7851f96849ce58dab5752d5e50225b84edc
Yunyao Zhu <yunyaozhu@Yunyaos-MacBook-Air.local>
2019-07-29 21:09:13 -0400

    test51

--- run.py
+++ run.py
@@ -285,6 +285,8 @@ def update_image_src(dataset_filename, dataset_options, metric_checklist, xaxis_
         'data': data,
         'layout': {
 #             'margin': dict(l=0, r=0, b=0, t=0, pad=0),
+            'height': 250,
+             'margin': { l: 50, r: 50, b: 45, t: 45, pad: 10},
             'title': 'Race Disparate Impact',
             'xaxis' : dict(
                 title='Epsilons',
@@ -317,7 +319,7 @@ def update_image_src(dataset_filename, dataset_options, metric_checklist, xaxis_
 #                     }
 #                 ],
             'legend_orientation':"h",
-            'legend':dict(x=-1.5, y=-1.5)
+            'legend':dict(x=0, y=-1.5)
         }
     }
     return figure
@@ -375,7 +377,7 @@ def update_image_src(dataset_filename, dataset_options, metric_checklist, xaxis_
                 )
             ),
             'legend_orientation':"h",
-            'legend':dict(x=-1.5, y=-1.5)
+            'legend':dict(x=0, y=-1.5)
         }
     }
     return figure
